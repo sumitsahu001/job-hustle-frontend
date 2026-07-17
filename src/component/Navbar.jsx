7c105ea8eea76b3ba761ee2536441fcff1aa158f
Sumit Sahu <sumitsahu9653@gmail.com>
2026-07-17 22:59:17 +0530

    created navbar

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,34 @@
+function Navbar(){
+return (
+
+<>
+<div >
+    <div className="logo">
+
+
+
+    </div>
+
+    <div className="navtext">
+<ul>
+    <li>Home</li>
+    <li>Features</li>
+    <li>About</li>
+    <li> Login</li>
+    <li>Get started</li>
+</ul>
+    </div>
+</div>
+
+
+
+
+</>
+
+)
+
+
+
+}
+
+export default Navbar;
